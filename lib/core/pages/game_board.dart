1b7f492ec3cf876632a47503f75e3d83b63ee3aa
XCreateev <gpflutter@gmail.com>
2023-11-26 06:58:56 +0500

    All pieces on position

--- lib/core/pages/game_board.dart
+++ lib/core/pages/game_board.dart
@@ -44,15 +44,90 @@ class _GameBoardState extends State<GameBoard> {
             imagePath: PieceIcons.pawnB);
       }
     // Place rooks
-
+    newBoard[0][0] = ChessPiece(
+        type: ChessPieceType.rook,
+        isWhite: false,
+        imagePath: PieceIcons.rookW
+    );
+    newBoard[0][7] = ChessPiece(
+        type: ChessPieceType.rook,
+        isWhite: false,
+        imagePath: PieceIcons.rookW
+    );
+    newBoard[7][0] = ChessPiece(
+        type: ChessPieceType.rook,
+        isWhite: true,
+        imagePath: PieceIcons.rookB
+    );
+    newBoard[7][7] = ChessPiece(
+        type: ChessPieceType.rook,
+        isWhite: true,
+        imagePath: PieceIcons.rookB
+    );
     // Place knights
-
+    newBoard[0][1] = ChessPiece(
+        type: ChessPieceType.knight,
+        isWhite: false,
+        imagePath: PieceIcons.knightW
+    );
+    newBoard[0][6] = ChessPiece(
+        type: ChessPieceType.knight,
+        isWhite: false,
+        imagePath: PieceIcons.knightW
+    );
+    newBoard[7][1] = ChessPiece(
+        type: ChessPieceType.knight,
+        isWhite: true,
+        imagePath: PieceIcons.knightB
+    );
+    newBoard[7][6] = ChessPiece(
+        type: ChessPieceType.knight,
+        isWhite: true,
+        imagePath: PieceIcons.knightB
+    );
     // Place bishops
-
+    newBoard[0][2] = ChessPiece(
+        type: ChessPieceType.bishop,
+        isWhite: false,
+        imagePath: PieceIcons.bishopW
+    );
+    newBoard[0][5] = ChessPiece(
+        type: ChessPieceType.bishop,
+        isWhite: false,
+        imagePath: PieceIcons.bishopW
+    );
+    newBoard[7][2] = ChessPiece(
+        type: ChessPieceType.bishop,
+        isWhite: true,
+        imagePath: PieceIcons.bishopB
+    );
+    newBoard[7][5] = ChessPiece(
+        type: ChessPieceType.bishop,
+        isWhite: true,
+        imagePath: PieceIcons.bishopB
+    );
     // Place queens
-
+    newBoard[0][4] = ChessPiece(
+        type: ChessPieceType.queen,
+        isWhite: false,
+        imagePath: PieceIcons.queenW
+    );
+    newBoard[7][4] = ChessPiece(
+        type: ChessPieceType.queen,
+        isWhite: true,
+        imagePath: PieceIcons.queenB
+    );
     // Place kings
-
+    newBoard[0][3] = ChessPiece(
+        type: ChessPieceType.king,
+        isWhite: false,
+        imagePath: PieceIcons.kingW
+    );
+    newBoard[7][3] = ChessPiece(
+        type: ChessPieceType.king,
+        isWhite: true,
+        imagePath: PieceIcons.kingB
+    );
     board = newBoard;
   }
 
